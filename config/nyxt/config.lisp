05726b43354dd0df9ba0dbd7919a19a28de6e318
VincentRAVERA <ravera.vincent@gmail.com>
2025-09-01 17:56:53 +0200

    Nyxt: visual-mode, query browser

--- config/nyxt/config.lisp
+++ config/nyxt/config.lisp
@@ -1,31 +1,9 @@
 ;;; Examples: https://github.com/aadcg/nyxt-config
 (in-package #:nyxt-user)
 
-;;; Tested with Nyxt 3.12.0
-;;; Navigation is with vim
-(define-configuration (buffer)
-    ;; All Buffers should use Default Vim bindings
-  ((default-modes
-       (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))
+;;; Developped with Nyxt 3.12.0
 
-   ;; Enforce M-x
-   (override-map
-    (let ((map (make-keymap "override-map")))
-      (define-key map
-          "M-x" 'execute-command)))
-   ))
-
-
-
-;; Owning back Space by removing the key in Every modes
-(defvar doom-free-space-key (list
-                             "space" 'nothing
-                             "C-space" 'nothing
-                             "C-M-space" 'nothing
-                             "C-shift-space" 'nothing
-                             "M-space" 'nothing))
-
-;; The new bindings
+;; The new global* bindings
 (defvar doom-normal-key (list
                              ;; "space" 'execute-command
                              "space space" 'switch-buffer
@@ -70,11 +48,14 @@
                              "space s o" 'doom-query-search-engine
                              ;; MISC
                              "space o r" 'repl
-                             ;; Workspaces
-                             ;; TODO Manage wokspace as groups of buffers as firefox
+                             ;; Workspaces/Context
+                             ;; TODO Manage wokspace as groups of buffers
                              ;; a la https://addons.mozilla.org/en-US/firefox/addon/simple-tab-groups/
-                             ;; This require a too much work, maybe some day
-                             ;; would be nice to  put those in the status line (see nyxt/source/status.lisp)
+                             ;; In 3.12.0 You can create contexts but this isn't generic and relies on rendrer
+                             ;; Any solution might break in >= 4.0.0 with electron for instance
+                             ;; nyxt:describe-function?fn=%1Bnyxt/renderer/gtk:make-buffer-with-context&function=%1Bnyxt/renderer/gtk:make-buffer-with-context
+                             ;; Misc
+                             "g i" 'focus-first-input-field ;; does not work?
                              ;; Vim Scrolls
                              "C-u" 'scroll-page-up
                              "C-d" 'scroll-page-down))
@@ -89,7 +70,7 @@
                              "K" 'expedition-next)))))
 
 ;; Remove it from status line
-(define-configuration reduce-tracking-mode
+(define-configuration (reduce-tracking-mode)
   ((visible-in-status-p nil)))
 
 ;; MissConfigured binding for document-mode
@@ -107,20 +88,53 @@
                              "K" 'expedition-next)
                             doom-normal-key)))))
 
+;; Visual mode bindings
+(define-configuration visual-mode
+  ((keyscheme-map
+    (define-keyscheme-map "visual-mode" (list :import %slot-value%)
+                            nyxt/keyscheme:vi-normal
+                            (list
+                             ;; No implementation of WORD
+                             ;; Added place holder to avoid misstype rage
+                             "W" 'forward-word
+                             "B" 'forward-word
+                             "E" 'forward-word
+                             "e" 'forward-word)))))
+
+;;; Navigation is with vim
+(define-configuration (buffer)
+    ;; All Buffers should use Default Vim bindings
+  ((default-modes
+       (pushnew 'nyxt/mode/vi:vi-normal-mode %slot-value%))
+
+   ;; Enforce M-x
+   (override-map
+    (let ((map (make-keymap "override-map")))
+      (define-key map
+          "M-x" 'execute-command)))
+   ))
+
 ;;; Prompt uses Vanilla Emacs bindings as vim does
 (define-configuration (prompt-buffer)
   ((default-modes (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Search Engine Prompt
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+(defvar doom-last-search-engine nil
+  "Latest Engine used via doom-query-search-engine")
 (define-command-global doom-query-search-engine (&key (query-in-new-buffer-p t))
   "Search prompted text using the queried search engine.
 QUERY-IN-NEW-BUFFER creates a new buffer with the search results."
   (let* ((selection (ffi-buffer-copy (current-buffer)))
          (engine (prompt1 :prompt "Search engine"
-                          :sources 'nyxt::search-engine-source))
+                          :input (if (not doom-last-search-engine)
+                                     ""
+                                     (slot-value doom-last-search-engine 'name))
+                          :sources (list
+                                    (make-instance 'nyxt::search-engine-source))
+                          ))
          (query (prompt1 :prompt "Query"
                         :input selection
                         :sources
@@ -130,14 +144,16 @@ QUERY-IN-NEW-BUFFER creates a new buffer with the search results."
                             (make-buffer-focus)
                             (current-buffer))))
     (when engine
+      (setq doom-last-search-engine engine)
       (buffer-load (make-instance 'nyxt:new-url-query :query query :engine engine)
                    :buffer target-buffer))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; User Defined DNS Blocking
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Add your Favourite DNS Blocking source
 ;; see nyxt:describe-class?class=%1Bnyxt/mode/blocker:blocker-mode
+;; Add your Favourite DNS Blocking source
+;; TODO: This is still lacking, need to investigate other solutions
 (defparameter additionals-blocking-url (list
                                         (make-instance 'nyxt/mode/blocker:hostlist
                                                        :url (quri:uri "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/pro.txt")
@@ -147,8 +163,6 @@ QUERY-IN-NEW-BUFFER creates a new buffer with the search results."
   "Blocker mode with custom hosts from *my-blocked-hosts*."
   ((nyxt/mode/blocker:hostlists additionals-blocking-url)
    (visible-in-status-p nil)))
-
-
 
 (define-configuration (buffer)
   ((default-modes
